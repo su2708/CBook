6fd292445bd8344f889e3061c7c4a4a2bc2f332c
su2708 <46125751+su2708@users.noreply.github.com>
2025-01-02 16:05:26 +0900

    Update: token/ -> login/

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,7 +10,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('', views.signup, name='signup'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
