999c4b7ef7e33647be9c879b6b2c1512c86a2db3
su2708 <46125751+su2708@users.noreply.github.com>
2025-01-02 19:00:35 +0900

    Update: change timezone

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     phone = models.CharField(max_length=13, blank=True, null=True)
     profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
     gender = models.BooleanField(default=True) #True='남자' False='여자'
-    last_login = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(auto_now=True)
     
     REQUIRED_FIELDS = ["phone"]
     
