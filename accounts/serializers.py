ff46491d903d0e34b47485a3846e4ffa52ff9034
su2708 <46125751+su2708@users.noreply.github.com>
2025-01-02 16:04:02 +0900

    Update: SignupSerializer 추가

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,27 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 
 User = get_user_model()
 
 # create Serializers
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'email', 'password', 'password2', 'username'
+        )
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({
+                "password": "비밀번호가 일치하지 않습니다."
+            })
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password2')  # password2 제거
+        return User.objects.create_user(**validated_data)
