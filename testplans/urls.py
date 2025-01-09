f71cc4f7b7e0388eb5ce81f41a285f688bcff68e
su2708 <46125751+su2708@users.noreply.github.com>
2025-01-10 00:48:25 +0900

    Update: FBV -> CBV

--- testplans/urls.py
+++ testplans/urls.py
@@ -6,5 +6,6 @@ app_name = 'testplans'
 urlpatterns = [
     path('', views.create_plan, name='create_plan'),
     path('user/<int:user_id>/', views.get_plan, name='get_plan'),
+    # path('<int:user_id>'/, PlanListView.as_view(), name="plan")
 ]
 
